472e83d79918d752d43014946fccd0603eb525ed
Abu <abubakr.ingush@gmail.com>
2021-07-10 16:10:33 +0300

    сделал модель комментарие, и контроллер

---
+++ models/Comment.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const commentSchema = new Schema({
+  text: { type: String, required: true },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: 'Note',
+    required: true
+  },
+}, {timestamps: true})
+const Comment = model('Comment', commentSchema)
+module.exports = Comment
