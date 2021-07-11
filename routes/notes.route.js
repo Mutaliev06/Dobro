473c19d3742083dae85279db2537c8ad1e0f90cf
mutaliev06 <maga_mutaliev06@mail.ru>
2021-07-11 14:53:09 +0300

    Mutaliev

--- routes/notes.route.js
+++ routes/notes.route.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/notes", notesController.getAllNotes);
-router.post('/notes', notesController.createNote)
-router.delete('/note/:id', notesController.deleteNote)
+router.post('/notes', notesController.createNote);
+router.delete('/note/:id', notesController.deleteNote);
 
 module.exports = router;
