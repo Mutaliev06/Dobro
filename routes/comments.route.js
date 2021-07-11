fbce9268d946226dcbcce78921449dde024a46c8
Abu <abubakr.ingush@gmail.com>
2021-07-11 16:34:20 +0300

    jr

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -6,3 +6,11 @@ router.post("/note/:id/", commentsControllers.createComment);
 router.delete("/comment/:id/", commentsControllers.deleteComment);
 
 module.exports = router;
+const { Router } = require("express");
+const router = Router();
+const { commentsControllers } = require("../controllers/comments.controller");
+router.get("/note/:id/comment", commentsControllers.getCommentNote);
+router.post("/note/:id/", commentsControllers.createComment);
+router.delete("/comment/:id/", commentsControllers.deleteComment);
+
+module.exports = router;
