8453c877e5f49db14b199ed332b32428b0ced9a4
mutaliev06 <maga_mutaliev06@mail.ru>
2021-07-14 09:54:36 +0300

    коммент

--- client/src/components/Main/SlideComponent.jsx
+++ client/src/components/Main/SlideComponent.jsx
@@ -2,18 +2,37 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Preloader from '../Preloader';
 import Slider from "react-slick";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  div: {
+    height: "495px"
+  },
+  img:{
+    width: "100%",
+    height: "100%"
+  },
+  slideComponent: {
+    marginBottom: "50px"
+  },
+  slider: {
+    maxWidth: "90%",
+    margin: 'auto'
+  }
+})
 
 export default function SlideComponent(props) {
+  const classes = useStyles();
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    width: "100%",
+    width: "90%",
     height: "500px",
     backgroundSize: "cover",
-    margin: "auto"
+    marginBottom: "50px"
   };
 
   const loading = useSelector((state) => state.notes.loading);
@@ -23,17 +42,17 @@ export default function SlideComponent(props) {
   }
 
   return (
-    <div>
+    <div className={classes.slideComponent}>
       <h2>Мероприятия</h2>
-      <Slider {...settings}>
-        <div>
-          <img src="https://volonter.ru/wp-content/uploads/Glavnaya_slayder_3_1-e1523304811430-1.jpg"/>
+      <Slider {...settings} className={classes.slider}>
+        <div className={classes.div}>
+          <img className={classes.img} src="https://volonter.ru/wp-content/uploads/Glavnaya_slayder_3_1-e1523304811430-1.jpg"/>
         </div>
-        <div>
-          <img src="https://volonter.ru/wp-content/uploads/Glavnaya_slayder_1_3-1-e1523285081114-1.jpg"/>
+        <div className={classes.div}>
+          <img className={classes.img} src="https://volonter.ru/wp-content/uploads/Glavnaya_slayder_1_3-1-e1523285081114-1.jpg"/>
         </div>
-        <div>
-          <img src="https://volonter.ru/wp-content/uploads/Glavnaya_slayder_3-1-e1523304754527-1.jpg"/>
+        <div className={classes.div}>
+          <img className={classes.img} src="https://volonter.ru/wp-content/uploads/Glavnaya_slayder_3-1-e1523304754527-1.jpg"/>
         </div>
       </Slider>
     </div>
